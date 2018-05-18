7f46222be8bc61dd691180ec6f78e06a2bf61f86
Nangxi <575981390@qq.com>
2018-05-18 16:51:56 +0800

    the first upload

---
+++ js/main.js
@@ -0,0 +1,121 @@
+window.onload=function(){
+	var bili=document.documentElement.clientWidth/750;
+	var canvatext=document.getElementById('myCanvatext');
+    var text=canvatext.getContext('2d');
+    text.scale(bili,bili);
+    text.beginPath();
+    text.font = "45px 黑体";  
+    text.fillStyle = "white";  
+    text.fillText("结果分析",40,140);
+    text.beginPath();
+    text.font = "25px 黑体";  
+    text.fillStyle = "white";  
+    text.fillText("本次测验用时  8:25",40,180);
+    text.beginPath();
+    text.font = "25px 黑体";  
+    text.fillStyle = "white";  
+    text.fillText("答题正确率  70%",40,210);
+
+	
+
+
+
+    var canvassea=document.getElementById('myCanvaSea');
+    var sea=canvassea.getContext('2d');
+
+
+    canvassea.width = canvassea.parentNode.offsetWidth;  
+    canvassea.height = canvassea.parentNode.offsetHeight;
+    //如果浏览器支持requestAnimFrame则使用requestAnimFrame否则使用setTimeout  
+    window.requestAnimFrame = (function(){  
+    return  window.requestAnimationFrame       ||  
+            window.webkitRequestAnimationFrame ||  
+            window.mozRequestAnimationFrame    ||  
+            function( callback ){  
+              window.setTimeout(callback, 1000 / 60);  
+            };  
+    })();  
+    // 波浪大小
+    var boHeight = canvassea.height / 10;
+    var posHeight = canvassea.height / 1.2;
+    //初始角度为0  
+    var step = 0;  
+    //定义三条不同波浪的颜色  
+    var lines = ["rgba(104,213,255, 1)",  
+                   "rgba(149,229,255, 1)"];  
+    function loop(){  
+        sea.clearRect(0,0,canvassea.width,canvassea.height);  
+        step++;  
+        //画3个不同颜色的矩形  
+        for(var j = lines.length - 1; j >= 0; j--) {  
+            sea.fillStyle = lines[j];  
+            //每个矩形的角度都不同，每个之间相差45度  
+            var angle = (step+j*50)*Math.PI/180;  
+            var deltaHeight   = Math.sin(angle) * boHeight;
+            var deltaHeightRight   = Math.cos(angle) * boHeight;  
+            sea.beginPath();
+            sea.moveTo(0, posHeight+deltaHeight);  
+            sea.bezierCurveTo(canvassea.width/2, posHeight+deltaHeight-boHeight, canvassea.width / 2, posHeight+deltaHeightRight-boHeight, canvassea.width, posHeight+deltaHeightRight);  
+            sea.lineTo(canvassea.width, canvassea.height);  
+            sea.lineTo(0, canvassea.height);  
+            sea.lineTo(0, posHeight+deltaHeight);  
+            sea.closePath();  
+            sea.fill();  
+        }
+        requestAnimFrame(loop);
+    }  
+    loop(); 
+
+    var pnum=document.getElementsByClassName('text').length;
+    for(var i=0;i<pnum;i++){
+    	document.getElementsByClassName('text')[i].style.fontSize=(36*(document.documentElement.clientWidth/750))+"px";
+    }
+
+
+
+
+    
+
+    //柱子移动的动画函数
+    function Move(target,height,delay){
+        target.delay(delay).animate({"height":height},1000);
+    }
+    $(window).scroll(function(){
+        if($(document).scrollTop()<1/2*($(".page_3").offset().top)||$(document).scrollTop()>4/5*($(".page_4").offset().top)){
+            $(".pic_1").eq(0).stop();
+            $(".pic_1").eq(1).stop();
+            $(".pic_1").eq(2).stop();
+            $(".pic_1").eq(3).stop();
+            $(".pic_2").eq(0).stop();
+            $(".pic_2").eq(1).stop();
+            $(".pic_2").eq(2).stop();
+            $(".pic_2").eq(3).stop();
+            $(".pic_1").eq(0).css("height","0rem");
+            $(".pic_1").eq(1).css("height","0rem");
+            $(".pic_1").eq(2).css("height","0rem");
+            $(".pic_1").eq(3).css("height","0rem");
+            $(".pic_2").eq(0).css("height","0rem");
+            $(".pic_2").eq(1).css("height","0rem");
+            $(".pic_2").eq(2).css("height","0rem");
+            $(".pic_2").eq(3).css("height","0rem");
+        }else{
+            $(".pic_1").eq(0).stop();
+            $(".pic_1").eq(1).stop();
+            $(".pic_1").eq(2).stop();
+            $(".pic_1").eq(3).stop();
+            $(".pic_2").eq(0).stop();
+            $(".pic_2").eq(1).stop();
+            $(".pic_2").eq(2).stop();
+            $(".pic_2").eq(3).stop();
+            Move($(".pic_1").eq(0),"1rem",0);
+            Move($(".pic_1").eq(1),"1.2rem",0);
+            Move($(".pic_1").eq(2),"0.7rem",0);
+            Move($(".pic_1").eq(3),"0.5rem",0);
+            Move($(".pic_2").eq(0),"1.3rem",0);
+            Move($(".pic_2").eq(1),"0.8rem",0);
+            Move($(".pic_2").eq(2),"1.3rem",0);
+            Move($(".pic_2").eq(3),"1.4rem",0);
+        }
+    })
+    
+}
